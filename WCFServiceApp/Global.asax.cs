b797dbc3e937a2e052714f3a7bcc7d1d38889a5e
thanhlai <thanhlai@outlook.com>
2014-11-18 11:55:22 -0800

    Added X-Auth-Token in allow header

--- WCFServiceApp/Global.asax.cs
+++ WCFServiceApp/Global.asax.cs
@@ -28,7 +28,7 @@ namespace WCFServiceApp
             {
                 //These headers are handling the "pre-flight" OPTIONS call sent by the browser
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Auth-Token");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
             }
